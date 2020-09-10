b99e76b7c358ffbaa4cc8eeec7b5b510323fc191
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-09-10 19:23:19 -0400

    validations and constraints for user model added

--- stockio/app/models/user.rb
+++ stockio/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true, length: {minimum:6, maximum:15}
+    validates :password, presence: true, length: {minimum:6}
+    validates :email, presence: true, uniqueness: true, format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 end
