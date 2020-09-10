f547c7df9d2cc1b994bb0770e84ea7ce928f0937
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-09-10 19:33:17 -0400

    added jwt token to response header

--- stockio/app/controllers/users_controller.rb
+++ stockio/app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
 
     before_action :authorize, except: [:signup, :login]
 
+    AUTHORIZATION_HEADER = "Authorization"
+    TOKEN_PREFIX = "Bearer"
+
     def signup
         user = User.create(user_params)
         if user.valid?
-            token = encode_token({user_id: user.id})
+            token = "#{TOKEN_PREFIX} #{encode_token({user_id: user.id})}"
+            response.headers[AUTHORIZATION_HEADER] = token
             render json: user, except: [:password_digest]
         else
             render json: {messages: user.errors.full_messages}, status: :internal_server_error
@@ -16,7 +20,8 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
 
         if user && user.authenticate(params[:password]) 
-            token = encode_token({user_id: user.id})
+            token = "#{TOKEN_PREFIX} #{encode_token({user_id: user.id})}"
+            response.headers[AUTHORIZATION_HEADER] = token
             render json: user, except: [:password_digest]
         else
             render json: {messages: user.error.full_messages}, status: :unauthorized
