3fce52c0bcd329a97b75396fdee615081e8c6e44
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-09-10 20:02:38 -0400

    user crud impl

--- stockio/app/controllers/users_controller.rb
+++ stockio/app/controllers/users_controller.rb
@@ -28,6 +28,26 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        render json: @user, except: :password_digest
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: @user, except: :password_digest
+        else
+            render json: {messages: @user.errors.full_messages}, status: :internal_server_error
+        end
+    end
+
+    def delete
+        if @user.destroy 
+            render json: {message: "account deleted"}
+        else
+            render json: {messages: @user.errors.full_messages}, status: :internal_server_error
+        end
+    end
+
     private 
 
     def user_params
--- stockio/config/routes.rb
+++ stockio/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post "/login", to: "users#login"
   post "/signup", to: "users#signup"
+  get "/user/show", to: "users#show"
+  put "/user/update", to: "users#update"
+  patch "/user/update", to: "users#update"
+  delete "/user", to: "users#delete"
 end
