ff9b05b29b4eca878742f44fa912b3f565cb5f24
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-09-11 10:27:55 -0400

    commenting routes

--- stockio/config/routes.rb
+++ stockio/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  ######### USER MANAGEMENT #############
   post "/login", to: "users#login"
   post "/signup", to: "users#signup"
   get "/user/show", to: "users#show"
   put "/user/update", to: "users#update"
   patch "/user/update", to: "users#update"
   delete "/user", to: "users#delete"
+  #######################################
+  resources :stocks
 end
